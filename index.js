699cc7fdc629a092cd09f299eb7f61db8a857bc9
vanesadeneb <vanessa_diesver@hotmail.com>
2022-10-13 09:51:34 -0700

    feat: joins two paths with path.join() method

--- index.js
+++ index.js
@@ -2,14 +2,18 @@ const fs = require('fs');
 const path = require('path');
 
 const mdLinks = (dir, options) => {
+  //Extension file
   const extensionFile = path.extname(dir);
-
+  //Join two paths
+  var joinPaths = path.join('/home/vanesa/Documents/developer/laboratoria', '/CDMX013-md-links');
+  console.log('Joining paths: ' + joinPaths);
+  //Read directory
   fs.readdir(__dirname, (err, files) => {
     files.forEach(file => {
       console.log(file);
     });
   });
-
+  //Read file
   fs.readFile(dir, 'utf8', (err, data) => {
     if (err) {
       console.error(err);
