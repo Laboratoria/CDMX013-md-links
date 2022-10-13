b30c80200fff3c27382f96ea51240bc6fb5cabf2
Arely Gutiérrez <yleraycnan@hotmail.com>
2022-10-13 17:20:03 -0500

    primer commit

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-module.exports = () => {
-  // ...
-};
+module.exports = function mdLinks(path, options){
+  return new Promise((resolve, reject) => {
+    resolve([
+      {
+        test: '',
+        file: '',
+        href: ''
+      }
+    ])
+  })
+}
