a3ee9db5f8978f99ab093e9380b85d48a7e0bb89
lizloang <liz.loang@gmail.com>
2022-10-14 10:51:07 -0500

    Add function to get absolute path and to get all md files

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
-const pathAbsolute = require("./lib/pathExist");
+const pathExist = require("./lib/pathExist");
+const pathAbsolute = require("./lib/absolutePath");
 const path = require("path");
+const fs = require("fs");
 let dir = process.argv[2];
 
 module.exports = () => {
-  console.log(pathAbsolute(dir));
+  dir = pathAbsolute(dir);
+  const results = [];
+  const openFolder = (dir) => {
+    list = fs.readdirSync(dir);
+    list.forEach((file) => {
+      let newDir = path.join(dir, file);
+      if (fs.lstatSync(newDir).isDirectory()) {
+        openFolder(newDir);
+      }
+
+      if (path.extname(newDir) == ".md") {
+        results.push(newDir);
+      }
+    });
+  };
+
+  if (pathExist(dir)) {
+    openFolder(dir);
+  }
+  console.log(results);
+  return results;
 };
 
 const mdLink = require("./index.js");
