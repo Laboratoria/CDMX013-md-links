94a8a0f1bfed22bbcced65ce80e2e6d0a2a53171
lizloang <liz.loang@gmail.com>
2022-10-13 19:31:37 -0500

    Add new function to know if file or folder exist

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+const pathAbsolute = require("./lib/pathExist");
+const path = require("path");
+let dir = process.argv[2];
+
 module.exports = () => {
-  // ...
+  console.log(pathAbsolute(dir));
 };
+
+const mdLink = require("./index.js");
+mdLink();
