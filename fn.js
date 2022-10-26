020d43f7005616d4ab219df949e9a2c7b76a7b2a
MayanSantiago17 <mayansantiago1703@gmail.com>
2022-10-25 22:37:28 -0500

    MDLS-28 <function convertPAthToAbsolute>

--- fn.js
+++ fn.js
@@ -4,11 +4,15 @@ const path = require('path');
 // saber si la ruta es absoluta
 const isPathAbsolute = (route) => path.isAbsolute(route);
 
+// convertir la ruta en absoluta
+const convertPathToAbsolute = (route) => path.resolve(route);
+
 
 // leer archivo
 const readFile = (route) => fs.readFileSync(route, "utf-8");
 
 module.exports = {
 readFile,
-isPathAbsolute
+isPathAbsolute,
+convertPathToAbsolute
 }
