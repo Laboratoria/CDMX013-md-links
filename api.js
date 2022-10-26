40e43f7199d148fa39696ba0227a028ce0bab820
MayanSantiago17 <mayansantiago1703@gmail.com>
2022-10-26 01:13:42 -0500

    MDLS-41 <init function getMdFile only extension of path>

---
+++ api.js
@@ -0,0 +1,16 @@
+/*
+const fn = require ('./fn.js');
+
+ 
+// GET .md FILE EXTENSION
+let allFiles = [];
+const mdFiles = allFiles.filter(file => {
+    if(functions.getMdFile(file) == '.md'){
+     return file;
+    }
+})
+
+module.exports = {
+mdFiles
+}
+*/
--- fn.js
+++ fn.js
@@ -1,5 +1,8 @@
 const fs = require ('fs');
 const path = require('path');
+const axios = require('axios');
+const http = require('http');
+
 
 // saber si la ruta es absoluta
 const isPathAbsolute = (route) => path.isAbsolute(route);
@@ -7,12 +10,21 @@ const isPathAbsolute = (route) => path.isAbsolute(route);
 // convertir la ruta en absoluta
 const convertPathToAbsolute = (route) => path.resolve(route);
 
-
 // leer archivo
 const readFile = (route) => fs.readFileSync(route, "utf-8");
 
+// obtener de archivos .md
+const getMdFile = (file) => path.extname(file);
+
+
+
+// validar links
+// const validateLinks = 
+
+
 module.exports = {
-readFile,
 isPathAbsolute,
-convertPathToAbsolute
+convertPathToAbsolute,
+readFile,
+getMdFile
 }
