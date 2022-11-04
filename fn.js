3e239b8b08ec606653017d37886cc417814b2464
MayanSantiago17 <mayansantiago1703@gmail.com>
2022-11-04 10:52:08 -0600

    MDLS-31 <function readFolders>

--- fn.js
+++ fn.js
@@ -4,12 +4,30 @@ const axios = require('axios');
 const http = require('http');
 
 
+
 // saber si la ruta es absoluta
 //pasandole route como parametro la hago pura
 const isPathAbsolute = (route) => path.isAbsolute(route);
 
 // convertir la ruta en absoluta
 const convertPathToAbsolute = (route) => path.resolve(route);
+
+// Es una carpeta?
+const isFolder = (route) => fs.lstatSync(route).isDirectory();
+
+// leer las carpetas
+const readFolders = (routePath, resultFiles = []) => {
+    const files = fs.readdirSync(routePath);
+    files.forEach((file) => {
+        const newPath = path.join(routePath, file);
+        if (fs.lstatSync(newPath).isDirectory()){
+            readFolders(newPath, resultFiles);
+        } else{
+            resultFiles.push(newPath)
+        };
+        })
+        return resultFiles;
+    }
 
 // leer archivo
 const readFile = (route) => fs.readFileSync(route, "utf-8");
@@ -26,6 +44,8 @@ const getMdFile = (file) => path.extname(file);
 module.exports = {
 isPathAbsolute,
 convertPathToAbsolute,
+isFolder,
+readFolders,
 readFile,
 getMdFile
 }
