1c770b934c734558bd38ff7c21d79798616e5d5b
Liz2208 <108889788+Liz2208@users.noreply.github.com>
2022-11-08 15:27:41 -0600

    hasta extraer los links md

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
 const fs = require('fs');
 const chalk = require('chalk');
 const path = require('path');
-const { argv } = require('node:process');
-const { items } = require('./mdLinks.js');
+//const { pathAbsolute } = require('./validapath.js');
+const { extraer } = require('./mdLinks');
+/*const { validaLinks } = require('./validateLinks.js');
+const { validation } = require('./promesa');*/
+//const { resolve } = require('path');
+//const { rejects } = require('assert');
 
-/*const fileName = argv[2];
-console.log(chalk.magenta(fileName));
-const resolverpath = path.resolve(fileName);
-console.log(resolverpath);*/
+
+const userPath = process.argv[2];
+//console.log(userPath);
 
 
-/*const review = file => {
-    const review = path.extname(file);
-if (review == '.md') {
-   
-    console.log(readArchivo(fileName));
-   } else {
-        console.log(chalk.red('NO ES ARCHIVO.md'));
+//valida que el archivo sea md 
+const mdLinks = (isMd) => {
+
+    const mdExtensionFiles = path.extname(isMd) === '.md'
+    if (mdExtensionFiles === false) {
+        console.log(chalk.red('ARCHIVO NO ES .md'));
+        console.log(chalk.red('BYE ..'));
+      
+    } else {
+        console.log(chalk.green('ARCHIVO SI ES .md'));
+        //pathAbsolute = (isMd);
+        extraer(userPath);
+       // validaLinks(userPath);
     }
 
-}*/
 
+}
+
+mdLinks(userPath);
--- mdLinks.js
+++ mdLinks.js
@@ -1,37 +1,45 @@
 const fs = require('fs');
 const chalk = require('chalk');
-//const path = require ('path');
+const path = require('path');
+//const fetch = require('node-fetch');
 
 
-//mostramos el directorio completo
-const items = fs.readdirSync('./node_modules', { withFileTypes: true });
+const userPath = process.argv[2];
+console.log(userPath);
 
-for (let item of items) {
-  const type = item.isDirectory() ? 'folder' : 'file';
-  console.log(chalk.blue(__dirname + `${item.name}: ${type}`));
-}
+//const validationLinks = require('./extraerLinks.js')
+//validationLinks (['https://nodejs.org/']);
+
+
+//verificamos si la ruta existe 
+//const extraerPath  =  fs.existsSync;
+//console.log(extraerPath(userPath) + '   verificamos ruta');
+
 
 //leemos el archivo y mostramos links
-const validate = (route) => {
-  const links = fs.readFileSync(route, 'utf-8').match(/\[(.+)\]\((https?:\/\/.+)\)/gi);
-  let list = links.map((link) => {
-   const limite =  link.indexOf('](');
-  
-    let obj = {
-      Title: link.substring(1, limite),
-      file: route,
-      Url: link.substring (limite +2, link.length -1 ),
-    }
-    return obj;
+const extraer = (userPath) => {
+   
+    const links = fs.readFileSync(userPath, 'utf-8').match(/\[(.+)\]\((https?:\/\/.+)\)/gi);
+    let list = links.map((link) => {
+        const limite = link.indexOf('](');
 
-  });
+        const obj = {
+            href: link.substring(limite + 2, link.length - 1),
+            text: link.substring(1, limite),
+            file: userPath,
+        }
+        return obj;
 
-  console.log(chalk.magenta('leyendo los links..'));
-  console.log(list);
-  return list;
+    });
+
+    console.log(chalk.magenta('leyendo los links..'));
+    console.log(list);
+    return list;
 }
-validate('README.md');
+
 
 module.exports = {
-  items
+    userPath,
+    extraer,
+
 }
