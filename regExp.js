4df2be1fac9cbc26fa9e7a0e76325f12c66610e1
Jane2do <107877817+Jane2do@users.noreply.github.com>
2022-11-08 21:58:09 -0600

    threeProperties

--- regExp.js
+++ regExp.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const fetch = require('node-fetch');
-//const fileReadme = './README.md';
+const fileReadme = './README.md';
 
 const pathText = path.join(__dirname, './readme.md');
 const readMe = fs.readFileSync(pathText, 'utf-8');
@@ -10,16 +10,19 @@ const readMe = fs.readFileSync(pathText, 'utf-8');
 const regex = /\[(.+)\]\((https?:.+)\)/ig;
 const linksArray = Array.from(readMe.match(regex))
 
-linksArray.forEach(texto => {
+const newArr =linksArray.map(texto => {
     const splitText = texto.split('(');
+    const linkText = splitText[0].replace('[', '').replace(']', '');
     const replaceText = splitText.pop().replace(')', '')
+return {href:replaceText, text:linkText, file: fileReadme}
 
-    fetch(replaceText).then(response => {
+    /*fetch(replaceText).then(response => {
         console.log(response.status)
-    });
+    });*/
 
 });
 //console.log(linksArray);
+console.log(newArr);
 
 
 
